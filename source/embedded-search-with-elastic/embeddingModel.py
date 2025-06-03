f8668f27dbe037bd48272ce5eef583f0264c8176
mohammadnasr <nasrmohammad4804@gmail.com>
2025-06-04 00:13:01 +0330

    remove python from root directory

--- source/embedded-search-with-elastic/embeddingModel.py
+++
@@ -1,9 +0,0 @@
-# bert_api.py
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed(sentence):
-
-    embeddings = model.encode(sentence).tolist()
-    return embeddings
